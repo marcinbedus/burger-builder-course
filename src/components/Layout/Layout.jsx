d9a5156a9b9a46a757cdf070373aad34ad841e94
MarcinBedus <marcin.bedu@gmail.com>
2020-07-09 14:06:39 +0200

    initial commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
+
+const layout = props => (
+    <Auxiliary>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Auxiliary>
+);
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildsControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+}
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        },
+        totalPrice: 4,
+        purchaseable: false,
+        purchasing: false,
+    }
+
+    updatePurchaseState() {
+        const ingredients = {
+            ...this.state.ingredients
+        };
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        this.setState({ purchaseable: sum > 0 })
+    }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredient = {
+            ...this.state.ingredients,
+        };
+        updatedIngredient[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredient }, () => this.updatePurchaseState());
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredient = {
+            ...this.state.ingredients,
+        };
+        updatedIngredient[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceAddition;
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredient }, () => this.updatePurchaseState());
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!')
+    }
+
+    render() {
+        const disabledInfo = {
+            ...this.state.ingredients,
+        }
+
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
+        return (
+            <Aux>
+                <Modal 
+                modalClosed={this.purchaseCancelHandler}
+                show={this.state.purchasing}>
+                    <OrderSummary 
+                    ingredients={this.state.ingredients}
+                    purchaseCanceled={this.purchaseCancelHandler}
+                    purchaseContinue={this.purchaseContinueHandler}
+                    />
+                </Modal>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls disabled={disabledInfo}
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    price={this.state.totalPrice}
+                    purchaseable={this.state.purchaseable}
+                    ordered={this.purchaseHandler} />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
